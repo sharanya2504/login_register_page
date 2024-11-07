d61ac3189a735bfa912568e2e49af9814e921f5e
sharanya <sharanyalakkamsetti@gmail.com>
2024-11-07 12:14:56 +0530

    code changes

--- login/src/Login.jsx
+++ login/src/Login.jsx
@@ -27,7 +27,7 @@ function Login() {
 
   return (
     <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
-      <div className="bg-white p-3 rounded w-25">
+      <div className="bg-white p-3 rounded w-50">
         <h2>Login</h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-3">
--- login/src/Signup.jsx
+++ login/src/Signup.jsx
@@ -1,3 +1,4 @@
+
 import { useState } from "react";
 import { Link } from 'react-router-dom';
 import axios from 'axios';
@@ -86,5 +87,3 @@ function Signup() {
 }
 
 export default Signup;
-
-
